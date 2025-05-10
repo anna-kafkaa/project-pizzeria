b7cc2800eb4b86bb04f36b10577f0303a4821479
Anna Kafka <anna.kafka933@gmail.com>
2025-05-10 13:57:35 +0200

    Implement full accordion toggle logic in initAccordion method

--- src/js/script.js
+++ src/js/script.js
@@ -83,9 +83,20 @@ const select = {
   initAccordion() {
     const thisProduct = this;
 
-    
-  }  
-}
+    const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
+    clickableTrigger.addEventListener('click', function(event) {
+      event.preventDefault();
+
+      const activeProduct = document.querySelector(select.all.menuProductsActive);
+
+      if (activeProduct && activeProduct !== thisProduct.element) {
+        activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+      }
+
+      thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+    });
+  }
 
   // ✅ Obiekt app
   const app = {
