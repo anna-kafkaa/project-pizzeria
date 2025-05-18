ea3270ccce1b79a59ec5e545b227c55e059dcb86
Anna Kafka <anna.kafka933@gmail.com>
2025-05-18 20:59:10 +0200

    Add AJAX fetch to load products from API instead of local data

--- dist/js/script.js
+++ dist/js/script.js
@@ -76,6 +76,12 @@ const settings = {
   cart: {
     defaultDeliveryFee: 20,
   },
+
+  db: {
+  url: '//localhost:3131',
+  products: 'products',
+  orders: 'orders',
+},
   
 };
 
@@ -501,15 +507,34 @@ prepareCartProductParams() {
 
   const app = {
     initData: function () {
-      const thisApp = this;
-      thisApp.data = dataSource;
-    },
+  const thisApp = this;
+
+  thisApp.data = {}; 
+
+  const url = settings.db.url + '/' + settings.db.products;
+
+  fetch(url)
+    .then(function (rawResponse) {
+      return rawResponse.json();
+    })
+    .then(function (parsedResponse) {
+      console.log('parsedResponse', parsedResponse);
+
+      // zapis danych do thisApp.data.products
+      thisApp.data.products = parsedResponse;
+
+      // uruchomienie initMenu po odebraniu danych
+      thisApp.initMenu();
+    });
+
+  console.log('thisApp.data', JSON.stringify(thisApp.data)); 
+},
 
     initMenu: function () {
       const thisApp = this;
 
-      for (let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
+      for (let productData of thisApp.data.products) {
+        new Product(productData.id, productData);
       }
     },
 
@@ -523,7 +548,6 @@ prepareCartProductParams() {
       const thisApp = this;
 
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
     },
   };
