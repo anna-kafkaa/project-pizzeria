7d8f2bee695ff55e8ba8dc91a1179d23e2d99493
Anna Kafka <anna.kafka933@gmail.com>
2025-06-16 18:42:18 +0200

    Changes in code

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,59 +1,107 @@
-import { settings, select } from './settings.js';
-import Product from './components/Product.js';
-import Cart from './components/Cart.js';
-  
-  const app = {
-    initData: function () {
-  const thisApp = this;
+import { settings, select, classNames } from "./settings.js";
+import Product from "./components/Product.js";
+import Cart from "./components/Cart.js";
+import Booking from './components/Booking.js';
 
-  thisApp.data = {}; 
+const app = {
+  initPages: function () {
+    const thisApp = this;
 
-  const url = settings.db.url + '/' + settings.db.products;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
 
-  fetch(url)
-    .then(function (rawResponse) {
-      return rawResponse.json();
-    })
-    .then(function (parsedResponse) {
-      console.log('parsedResponse', parsedResponse);
+    const idFromHash = window.location.hash.replace('#/', '');
+    let pageMatchingHash = thisApp.pages[0].id;
 
-      // zapis danych do thisApp.data.products
-      thisApp.data.products = parsedResponse;
-
-      // uruchomienie initMenu po odebraniu danych
-      thisApp.initMenu();
-    });
-
-  console.log('thisApp.data', JSON.stringify(thisApp.data)); 
-},
-
-    initMenu: function () {
-      const thisApp = this;
-
-      for (let productData of thisApp.data.products) {
-        new Product(productData.id, productData);
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
       }
-    },
+    }
 
-    initCart: function () {
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener("click", function (event) {
+        event.preventDefault();
+        const id = link.getAttribute("href").replace("#", "");
+        thisApp.activatePage(id);
+        window.location.hash = "#/" + id;
+      });
+    }
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id === pageId);
+    }
+
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute("href") === "#" + pageId
+      );
+    }
+  },
+
+  initData: function () {
+    const thisApp = this;
+
+    thisApp.data = {};
+
+    const url = settings.db.url + "/" + settings.db.products;
+
+    fetch(url)
+      .then(function (rawResponse) {
+        return rawResponse.json();
+      })
+      .then(function (parsedResponse) {
+        console.log("parsedResponse", parsedResponse);
+
+        thisApp.data.products = parsedResponse;
+        thisApp.initMenu();
+      });
+
+    console.log("thisApp.data", JSON.stringify(thisApp.data));
+  },
+
+  initMenu: function () {
+    const thisApp = this;
+
+    for (let productData of thisApp.data.products) {
+      new Product(productData.id, productData);
+    }
+  },
+
+  initBooking: function () {
+    const thisApp = this;
+
+    const bookingContainer = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingContainer);
+  },
+
+  initCart: function () {
     const thisApp = this;
     const cartElem = document.querySelector(select.containerOf.cart);
     thisApp.cart = new Cart(cartElem);
 
-    thisApp.productList = document.querySelector(select.containerOf.menu)
-    thisApp.productList.addEventListener('add-to-cart', function(event){
+    thisApp.productList = document.querySelector(select.containerOf.menu);
+    thisApp.productList.addEventListener("add-to-cart", function (event) {
       app.cart.add(event.detail.product);
-    })
-    },
+    });
+  },
 
-    init: function () {
-      const thisApp = this;
+  init: function () {
+    const thisApp = this;
 
-      thisApp.initData();
-      thisApp.initCart();
-    },
-  };
+    thisApp.initPages();
+    thisApp.initData();
+    thisApp.initCart();
+    thisApp.initBooking();
+  },
+};
 
-  app.init();
-
-
+app.init();
